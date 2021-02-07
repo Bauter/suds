988520507cb1095a576480b06c4ce1851a15b954
Bauter <bauter09@gmail.com>
2021-02-07 17:59:24 -0600

    removed console.log from JS file

--- assets/app.js
+++ assets/app.js
@@ -3,13 +3,11 @@ $(document).ready(function() {
     $(".card").mouseover(function(event) {
         event.preventDefault();
         $(this).children("img.shirtImg").css({"opacity":".6"})
-        console.log("FOUND")
     });
 
     $(".card").mouseleave(function(event) {
         event.preventDefault();
         $(this).children("img.shirtImg").css({"opacity":"1"})
-        console.log("FOUND")
     });
 
 
